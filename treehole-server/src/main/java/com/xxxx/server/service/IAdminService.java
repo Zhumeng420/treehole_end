aababb093e73073759b5532c3a1c9af5ea845daf
zhumeng <zhumeng@cug.edu.cn>
2022-04-01 14:54:38 +0800

    Initial commit

---
+++ treehole-server/src/main/java/com/xxxx/server/service/IAdminService.java
@@ -0,0 +1,36 @@
+package com.xxxx.server.service;
+
+//import com.xxxx.pojo.Admin;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.server.pojo.Admin;
+import com.xxxx.server.pojo.RespBean;
+//import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhoubin
+ * @since 2022-03-29
+ */
+public interface IAdminService extends IService<Admin> {
+    /**
+     * 登录之后返回token
+     * @param username
+     * @param password
+     * @param code
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, String code, HttpServletRequest request);
+
+    /**
+     * 根据用户名获取用户
+     * @param username
+     * @return
+     */
+    Admin getAdminByUserName(String username);
+}
